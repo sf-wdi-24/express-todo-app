afccbe45532d75b864dc1fd9dcb4c23d48905ba7
cameronjacoby <cameron.jacoby@gmail.com>
2015-11-06 05:40:10 -0800

    update tests to get singleTodo dynamically

--- test/todosTest.js
+++ test/todosTest.js
@@ -4,26 +4,15 @@ var request = require('request'),
     expect = require('chai').expect,
     baseUrl = 'http://localhost:3000';
 
-describe('GET /', function() {
-  it('should return statusCode 200', function (done) {
-    request(baseUrl, function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-});
-
-describe('GET /api/todos', function() {
-  it('should return statusCode 200', function (done) {
+describe('Todos', function() {
+  it('should list ALL todos on GET /api/todos', function (done) {
     request(baseUrl + '/api/todos', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
-});
 
-describe('POST /api/todos', function() {
-  it('should return statusCode 200', function (done) {
+  it('should add a NEW todo on POST /api/todos', function (done) {
     request.post(
       {
         url: baseUrl + '/api/todos',
@@ -38,31 +27,46 @@ describe('POST /api/todos', function() {
       }
     );
   });
-});
 
-describe('PUT /api/todos/:id', function() {
-  it('should return statusCode 200', function (done) {
-    request.put(
-      {
-        url: baseUrl + '/api/todos/1',
-        form: {
-          task: 'Walk Dog',
-          description: 'Take Spot for a walk'
-        }
-      },
-      function (error, response, body) {
+  it('should list a SINGLE todo on GET /api/todo/:id', function (done) {
+    request(baseUrl + '/api/todos', function (error, response, body) {
+      var allTodos = JSON.parse(body).todos;
+      var singleTodo = allTodos[allTodos.length - 1];
+      request(baseUrl + '/api/todos/' + singleTodo._id, function (error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
-      }
-    );
+      });
+    });
   });
-});
 
-describe('DELETE /api/todos/:id', function() {
-  it('should return statusCode 200', function (done) {
-    request.del(baseUrl + '/api/todos/1', function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+  it('should update a SINGLE todo on PUT /api/todos/:id', function (done) {
+    request(baseUrl + '/api/todos', function (error, response, body) {
+      var allTodos = JSON.parse(body).todos;
+      var singleTodo = allTodos[allTodos.length - 1];
+      request.put(
+        {
+          url: baseUrl + '/api/todos/' + singleTodo._id,
+          form: {
+            task: 'Walk Dog',
+            description: 'Take Spot for a walk'
+          }
+        },
+        function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          done();
+        }
+      );
+    });
+  });
+
+  it('should delete a SINGLE todo on DELETE /todos/:id', function (done) {
+    request(baseUrl + '/api/todos', function (error, response, body) {
+      var allTodos = JSON.parse(body).todos;
+      var singleTodo = allTodos[allTodos.length - 1];
+      request.del(baseUrl + '/api/todos/' + singleTodo._id, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
   });
 });
