597d9795b159fdc7159d6f345667d754070de4c6
cameronjacoby <cameron.jacoby@gmail.com>
2015-11-05 20:53:45 -0800

    implement api routes for todos

--- server.js
+++ server.js
@@ -13,52 +13,91 @@ app.use(express.static(__dirname + '/public'));
 // set view engine to ejs
 // app.set('view engine', 'ejs');
 
-// our "database" is an array for now
-var todos = [];
+
+// pre-seeded todo data; our "database" is an array for now
+var todos = [
+  {id: 1, task: 'Laundry', description: 'Clean clothes'},
+  {id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week'},
+  {id: 3, task: 'Homework', description: 'Make this app super awesome!'}
+];
 
 
 // API ROUTES
 
 // get all todos
 app.get('/todos', function (req, res) {
+  // send all todos as JSON response
   res.json({ todos: todos });
 });
 
 // create new todo
 app.post('/todos', function (req, res) {
+  // create new todo with form data (`req.body`)
   var newTodo = req.body;
+  
+  // set sequential id (last id in `todos` array + 1)
+  if (todos.length > 0) {
+    newTodo.id = todos[todos.length - 1].id + 1;
+  } else {
+    newTodo.id = 1;
+  }
+
+  // add newTodo to `todos` array
   todos.push(newTodo);
+
+  // send newTodo as JSON response
   res.json(newTodo);
 });
 
 // get one todo
 app.get('/todos/:id', function (req, res) {
-  var todoId = req.params.id;
-  var todo = todos.filter(function (todo) {
+  // get todo id from url params (`req.params`)
+  var todoId = parseInt(req.params.id);
+
+  // find todo to by its id
+  var foundTodo = todos.filter(function (todo) {
     return todo.id == todoId;
   })[0];
-  res.json(todo);
+
+  // send foundTodo as JSON response
+  res.json(foundTodo);
 });
 
 // update todo
 app.put('/todos/:id', function (req, res) {
-  var todoId = req.params.id;
-  var todo = todos.filter(function (todo) {
+  // get todo id from url params (`req.params`)
+  var todoId = parseInt(req.params.id);
+
+  // find todo to update by its id
+  var todoToUpdate = todos.filter(function (todo) {
     return todo.id == todoId;
   })[0];
-  var updatedTodo = req.body;
-  todos.splice(todos.indexOf(todo), 1, updatedTodo);
-  res.json(updatedTodo);
+
+  // update the todo's task
+  todoToUpdate.task = req.body.task;
+
+  // update the todo's description
+  todoToUpdate.description = req.body.description;
+
+  // send back updated todo
+  res.json(todoToUpdate);
 });
 
 // delete todo
 app.delete('/todos/:id', function (req, res) {
-  var todoId = req.params.id;
-  var todo = todos.filter(function (todo) {
+  // get todo id from url params (`req.params`)
+  var todoId = parseInt(req.params.id);
+
+  // find todo to delete by its id
+  var todoToDelete = todos.filter(function (todo) {
     return todo.id == todoId;
   })[0];
-  todos.splice(todos.indexOf(todo), 1);
-  res.json(todo);
+  
+  // remove todo from `todos` array
+  todos.splice(todos.indexOf(todoToDelete), 1);
+  
+  // send back deleted todo
+  res.json(todoToDelete);
 });
 
 // listen on port 3000
